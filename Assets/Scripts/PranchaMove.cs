9fb8559847935b292d90f62dc85cc38b537eeb12
Aluno <aluno@udicentro.local>
2017-02-22 17:16:10 -0300

    Acrescentando retorno da prancha

--- Assets/Scripts/PranchaMove.cs
+++ Assets/Scripts/PranchaMove.cs
@@ -5,12 +5,10 @@ public class PranchaMove : MonoBehaviour {
 
 
     public float velocidade;
-
-
-
+    public float limite;
+    public float retorno;
 
 	void Start () {
-		
 	}
 	
 
@@ -20,9 +18,13 @@ public class PranchaMove : MonoBehaviour {
         Vector3 velocidadeVetorial =  Vector3.left * velocidade;
         transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
 
+        if (transform.position.x <= limite) {
+            transform.position = new Vector3(retorno, transform.position.y, transform.position.z);
 
 
 
+
+        }
 
     }
 }
