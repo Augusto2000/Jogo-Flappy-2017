a7b486d9d6cd76df44b8933257dd21040fb5873c
Augusto Melo <augustinho.melo@hotmail.com>
2017-03-21 11:37:10 -0300

    Controlando os estados do jogo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
+    public Estado estado { get; private set; }
+
     public GameObject obstaculo;
     public float espera;
     public float tempoDestruicao;
@@ -26,21 +28,26 @@ public class GameController : MonoBehaviour
     }
 
 
-    void Start()
-    {
-        StartCoroutine(GerarObstaculos());
-
+    void Start() {
+        estado = Estado.AguardandoComecar;
     }
 
 
-    IEnumerator GerarObstaculos()
-    {
-        while (true)
-        {
+    IEnumerator GerarObstaculos() {
+        while (GameController.instancia.estado == Estado.Jogando) {
             Vector3 pos = new Vector3(6.6f, Random.Range(0f, 5.0f), -8.05f);
             GameObject obj = Instantiate(obstaculo, pos, Quaternion.Euler(0f,-90f,0f)) as GameObject;
             Destroy(obj, tempoDestruicao);
             yield return new WaitForSeconds(espera);
         }
+    }
+
+    public void PlayerComecou() {
+        estado = Estado.Jogando;
+        StartCoroutine(GerarObstaculos());
+    }
+
+    public void PlayerMorreu() {
+        estado = Estado.GameOver;
     }
 }
